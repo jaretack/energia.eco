b5895c6a41d9fffab76ca6c43f9a4940e3ed371a
Jarosław Zdyb <hello@jaroslawzdyb.dev>
2020-03-20 18:49:40 +0100

    Added 7.1 sass architecture

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Energia.eco</title>
+
+    <link href="{{ elixir('css/main.css') }}" rel="stylesheet">
+</head>
+
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <!-- Navbar Contents -->
+    </nav>
+</div>
+
+<script src="{{ elixir('js/app.js') }}"></script>
+</body>
+</html>
